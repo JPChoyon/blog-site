37337052d046993ae5e27ef46b137ddfad837b53
JP Choyon Khan <kotjpckhan@gmail.com>
2024-01-29 23:52:01 +0600

    clean code for the blog website ready to start

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        satoshi: ['Satoshi', 'sans-serif',],
-        inter: ['inter', 'sans-serif'],
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        'primary-orange': '#FF5722'
-      }
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
